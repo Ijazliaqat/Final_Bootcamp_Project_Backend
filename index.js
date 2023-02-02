c783ec74be470bae1548a1c851a9a74dd71fbb11
Ijaz Liaqat <ijazue6@gmail.com>
2023-02-02 18:22:31 +0500

    #comment post and get api working

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const bodyParser = require('body-parser')
 const Products = require('./dbProducts/dbProducts')
 const app = express();
 const connectDB = require('./db/connect');
 const PORT = 9000;
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 app.get('/all-products', async (req, res) => {
 
@@ -18,29 +20,38 @@ app.get('/all-products', async (req, res) => {
         res.status(500).send(error)
     }
 })
+// UPPERCASE
+// ADD_PRODUCT // Environment Variables / App-wide Constants example: BASE_URL
+// snake_case
+// add_product_to_cart // Python
+// camelCase
+// addProductToCart // JavaScript
+// kebab-case
+// add-product-to-cart // API Routes
+// PascalCase
+// AddProductToCart
+// Title Case
+// Add Product To Cart // Heading
+// Sentence case
+// Add product to cart // Paragraphs
 
-app.post('/addproducts', async (req, res, next) => {
-    try {
-        const { name, oldPrice, newPrice, image } = req.body
-        const product = new Products({ data });
-        await product.save();
-        res.status(201).json({ message: "Product created successfully", product });
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
 
+app.post('/add-product', async (req, res, next) => {
+    console.log(req.body);
+    const dataObj = req.body
+    Products.create(dataObj, (err, dataObj) => {
+        if (err) {
+            res.status(400).json({ message: err.message });
+            // res.status(500).send(err)
+        } else {
+            // res.send(dataObj)
+            res.status(201).json({ message: "Product created successfully", data: dataObj, errors: null });
+        }
+    })
+})
 
-    console.log({ data });
-
-    // Products.create({ product }, (err, product) => {
-    //     if (err) {
-    //         res.status(400).json({ message: err.message });
-    //         // res.status(500).send(err)
-    //     } else {
-    //         res.send(product)
-    //         res.status(201).json({ message: "Product created successfully", product });
-    //     }
-    // })
+app.use('/',(req,res)=>{
+    res.send('express')
 })
 
 const start = async () => {
